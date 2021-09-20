edf01028b9f49d121ab9517ebec97e4773ce2120
Matthew Pierce <lesbianhaircut@outlook.com>
2021-09-20 19:18:05 -0400

    c

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 window.onload = () => {
   // Variables
-  const buttons = document.querySelectorAll(".button");
+
+  const buttons = document.querySelectorAll("button");
+  const emailText = document.querySelectorAll(".email-text");
   // Functions
+  function checkEmail(emailField) {}
 
   // Event listeners
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      let regex = /[0-9]/g;
+      let id = e.currentTarget.id.match(regex);
+      console.log(e.currentTarget);
+    });
+  });
 };
